3f7dc7f58b7d1ad37485b4c459c6e0687f02e4f6
Matt Netkow <matt.netkow@gmail.com>
2020-05-17 19:32:24 -0500

    move to env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,60 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { IonicAuthOptions } from '@ionic-enterprise/auth';
+
+export const azureNativeConfig : IonicAuthOptions = {
+  // the auth provider
+  authConfig: 'azure',
+  // The platform which we are running on
+  platform: 'capacitor',
+  // client or application id for provider
+  clientID: 'b69e2ee7-b67a-4e26-8a38-f7ca30d2e4d4',
+  // the discovery url for the provider
+  // OpenID configuration
+  discoveryUrl: 'https://vikingsquad.b2clogin.com/vikingsquad.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_Signup_Signin',
+  // the URI to redirect to after log in
+  redirectUri: 'FILL-IN',
+  // requested scopes from provider
+  scope: 'openid offline_access email profile https://vikingsquad.onmicrosoft.com/api/Hello.Read',
+  // the audience, if applicable
+  audience: 'FILL-IN',
+  // the URL to redirect to after log out
+  logoutUrl: 'FILL-IN',
+  // The type of iOS webview to use. 'shared' will use a webview that can share session/cookies
+  // on iOS to provide SSO across multiple apps but will cause a prompt for the user which asks them
+  // to confirm they want to share site data with the app. 'private' uses a webview which will not
+  // prompt the user but will not be able to share session/cookie data either for true SSO across
+  // multiple apps.
+  iosWebView: 'private'
+};
+
+export const azureWebConfig : IonicAuthOptions = {
+  // the auth provider
+  authConfig: 'auth0',
+  // The platform which we are running on
+  platform: 'web',
+  // client or application id for provider
+  clientID: 'b69e2ee7-b67a-4e26-8a38-f7ca30d2e4d4',
+  // the discovery url for the provider
+  // OpenID configuration
+  discoveryUrl: 'FILL-IN',
+  // the URI to redirect to after log in
+  redirectUri: 'FILL-IN',
+  // requested scopes from provider
+  scope: 'openid offline_access email picture profile',
+  // the audience, if applicable
+  audience: 'FILL-IN',
+  // the URL to redirect to after log out
+  logoutUrl: 'FILL-IN',
+  // The type of iOS webview to use. 'shared' will use a webview that can share session/cookies
+  // on iOS to provide SSO across multiple apps but will cause a prompt for the user which asks them
+  // to confirm they want to share site data with the app. 'private' uses a webview which will not
+  // prompt the user but will not be able to share session/cookie data either for true SSO across
+  // multiple apps.
+  iosWebView: 'private',
+  implicitLogin: 'CURRENT'
+};
 
 export const environment = {
   production: false
